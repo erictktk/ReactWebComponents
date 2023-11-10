c060db2e034b42cf2bfebb2d693cb5aa6438a8de
Erik Tkachuk <erictktk@gmail.com>
2023-11-09 23:14:36 -0500

    tried to rearrange structure

--- params.js
+++ params.js
@@ -1,9 +1,5 @@
 //parameters
-
-
 //Prompting string:
-
-
 export const promptingString = `This is a concatenation of files in a project I'm working.  I need you to see this code and
 analyze it (without commenting on it) so you can help with suggestions as to how to write further code when I prompt.`;
 
